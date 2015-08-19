8abde6a44d59f8fe322f6f5f1a658cebba1a9fc0
rodcope1 <rod@perfdog.com>
2015-08-19 15:32:44 -0600

    Update package.js

--- package.js
+++ package.js
@@ -7,10 +7,10 @@ Package.describe({
 
 Package.on_use(function(api) {
   if (api.versionsFrom) {
-    api.use('mrt:moment-timezone@0.2.1');
-    api.use('aldeed:geocoder@0.3.1');
-    api.use('underscore@1.0.0');
-    api.use('http@1.0.0');
+    api.use('aldeed:moment-timezone@0.4.0');
+    api.use('aldeed:geocoder@0.3.6');
+    api.use('underscore@1.0.3');
+    api.use('http@1.1.0');
   } else {
     api.use(['moment-timezone', 'geocoder', 'underscore', 'http']);
   }
@@ -22,7 +22,7 @@ Package.on_use(function(api) {
 Package.on_test(function(api) {
   if (api.versionsFrom) {
     api.use('aldeed:timezoned');
-    api.use('tinytest@1.0.0');
+    api.use('tinytest@1.0.5');
   } else {
     api.use(['timezoned', 'tinytest']);
   }
